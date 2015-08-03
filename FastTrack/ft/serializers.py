d6eecf71811f09c5b73fe57ad8480c54c4d0da30
g3yeesta <hong.yee@mail.utoronto.ca>
2015-08-03 15:17:43 -0400

    delete serializers

--- FastTrack/ft/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import *
-#from django.contrib.auth.models import User
-
-class CustomerListingSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomerListing
-        fields = ('customerListingID', 'startLocation', 'endLocation', 'arrivalTime', 'status', 'poster')
